27bbf5d968250912cad01f2c29254695dc94532e
mrizwanashiq <mrizwanashiq@outlook.com>
2023-04-08 08:59:05 +0500

    feat

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 import express from "express";
+
 const app = express();
+
 app.use(express.json());
+
 const users = [];
+
 app.get("/", (req, res) => {
 	res.send(users);
 });
+
 app.post("/register", (req, res) => {
 	const user = req.body;
 	users.push(user);
 	res.send("users has been register");
 });
+
 app.post("/login", (req, res) => {
 	const user = req.body;
 	let isFound = false;
@@ -24,4 +30,5 @@ app.post("/login", (req, res) => {
 		res.send("user not found");
 	}
 });
+
 app.listen(3000);
